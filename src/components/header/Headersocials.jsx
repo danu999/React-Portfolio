aa922d61c97b861ad226e9706bde69fad98f53c5
danu999 <danusaputraoffice@gmail.com>
2023-05-30 09:36:27 +0700

    First-Commit

---
+++ src/components/header/Headersocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithubSquare} from 'react-icons/fa'
+import {FaDribbbleSquare} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+      <a href="https://linkeind.com" target="_blank"><FaLinkedin/></a>
+      <a href="https://github.com" target="_blank"><FaGithubSquare/></a>
+      <a href="https://dribbble.com" target="_blank"><FaDribbbleSquare/></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
